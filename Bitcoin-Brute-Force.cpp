c19d7f054307ccb74972b5da70b3ec8e4d25f57c
Domic <dominicbertocchini@gmail.com>
2021-01-26 18:42:29 -0800

    Add project files.

---
+++ Bitcoin-Brute-Force.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+using namespace std;
+
+class longint {
+public:
+	bool arr[256];
+
+	bool operator == (const longint &ref) const {
+		return arr == ref.arr;
+	}
+
+	longint operator + (const longint& ref) const {
+
+	}
+
+	longint operator - (const longint& ref) const {
+
+	}
+
+	longint operator * (const longint& ref) const {
+
+	}
+
+	longint operator ^ (const longint& ref) const {
+
+	}
+
+	longint operator % (const longint& ref) const {
+
+	}
+
+	string toHexadecimal() {
+
+	}
+
+	longint(string hexadecimal) {
+
+	}
+
+	longint() {
+		for (auto i : arr)
+			i = 1;
+	}
+};
+
+class point {
+public:
+	longint x;
+	longint y;
+
+	bool operator == (const point& ref) const {
+		return x == ref.x && y == ref.y;
+	}
+	
+	point negative() {
+
+	}
+
+	point(longint X, longint Y) {
+		x = X;
+		y = Y;
+	}
+	point() = default;
+};
+
+/*
+def point_add(P_x, P_y, Q_x, Q_y):
+	if P_x == O:
+		return Q_x, Q_y
+	if Q_x == O:
+		return P_x, P_y
+	if Q_x == P_x:
+		if Q_y == P_y:
+			Lambda = 3 * P_x**2 * pow(2 * P_y, p - 2, p)
+		elif Q_y == (-P_y) % p:
+			return O, O
+	else:
+		Lambda = (Q_y - P_y) * pow(Q_x - P_x, p - 2, p)
+	x_r = (Lambda**2 - (P_x + Q_x)) % p
+	y_r = (Lambda * (P_x - x_r) - P_y) % p
+	return x_r, y_r
+*/
+
+
+point add(point& P, point& Q, longint &p) {
+	longint lambda;
+	if (P.x == longint())
+		return Q;
+	if (Q.x == longint())
+		return P;
+	if (Q.x == P.x) {
+		if (Q.y == P.y)
+			lambda = 3 * P.x * P.x * ((Q.x - P.x) ^ p - 2);
+		else if (Q.y == (p - P.y))
+			return point();
+		else
+			lambda = (Q.y - P.y) * ((Q.x - P.x) ^ p - 2);
+		longint x = (lambda * lambda - (P.x + Q.x)) % p;
+		return point(x, (lambda * (P.x - x) - P.y) % p);
+	}
+}
+
+point multiply(longint &scalar, point P, longint &p) {
+	point q;
+	for (auto i = 255; i >= 0; i--) {
+		if (scalar.arr[i])
+			q = add(P, q, p);
+		P = add(P, P, p);
+	}
+	return q;
+}
+
+void main()
+{
+	//prime charcateristic
+	const longint p = longint("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F");
+
+	//base point
+	const point(longint("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798"),
+		longint("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8"));
+
+	//order of base point; n* G = O;
+	const longint n = longint("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141");
+
+    std::cout << "Hello World!\n";
+}
